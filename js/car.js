64b6da26c07303e33c161aa6e182d673df773a1a
mj <603535738@qq.com>
2017-03-02 14:27:46 +0800

    merge

--- js/car.js
+++ js/car.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-    //使用ajax方法读取json数据,
-    //注意：info.json可以是不同类型文件，只要其中的数据为json类型即可
-    $.ajax({
-        url: '../server/car-management/car.php',
-        dataType: 'json',
-        success: function(data) {
-          console.log(data);
-            var html_resultinfo;
-            $.each(data["records"], function(i, item) {
-                html_resultinfo = '';
-                html_resultinfo += '<div class="single-member effect-' + item['recid'] + '"><div class="member-info"><h3>' +
-                    item['plateNumber'] + '</h3><h5>' + item['brand'] +
-                    '</h5></div><div class="member-image"><img src="' +
-                    item["imageSrc"] + '" alt="Member"></div><div class="more-info"><p>型号：' +
-                    item['model'] + '</br>座位数：' + item['seating'] +
-                    '座</br>级别：' + item['level'] + '</br>变速箱：' + item['gearbox'] +
-                    '</br>汽油：' + item['gasoline'] + '</p><div class="social-touch icon-colored"><button class="fa fa-trash-o" id="delete-effect-' +
-                    item['recid'] + '"></button><button class="fa fa-pencil" id="edit-effect-' +
-                    item['recid'] + '"></button></div></div></div>';
-                $('.row').append(html_resultinfo); //after方法:在每个匹配的元素之后插入内容。
-            });
-            $('.fa-trash-o').click(function(e) {
-                var effectId = (e.target.id).substr(7, 8);
-                $('.' + effectId).remove();
-            });
-            $('.fa-pencil').click(function(e) {
-                var id = (e.target.id).substr(12, 1);
-                console.log(id);
-                window.location.href = 'detial.html?' + id;
-            });
-        },
-        error: function(xhr, textStatus) {
-            alert('出错啦！');
-            console.log('错误');
-            console.log(xhr);
-            console.log(textStatus);
-        }
-    });
-});
-=======
 $(document).ready(function(){
 //使用ajax方法读取json数据,
 //注意：info.json可以是不同类型文件，只要其中的数据为json类型即可
@@ -78,5 +36,3 @@ $(document).ready(function(){
 	  }
 	});
 });	
-			
->>>>>>> refs/remotes/origin/master
