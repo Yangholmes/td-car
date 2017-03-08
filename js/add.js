29ef3d147275cc3a2491370a112e104a7678ab6e
mj <603535738@qq.com>
2017-03-08 10:19:12 +0800

    选择过滤input的文件类型为image

--- js/add.js
+++ js/add.js
@@ -1,37 +1,40 @@
-$("#imageSrc").on("change", function() {
-    if (window.FileReader) {
-        var reader = new FileReader();
-    } else {
-        alert("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
-    }
-    // Get a reference to the fileList
-    var files = !!this.files ? this.files : [];
-    var preview = $("#td-form-field-preview");
-    // If no files were selected, or no FileReader support, return
-    if (!files.length) {
-        alert("请选择文件！");
-        //没有选择文件则要清空文件上一次load的文件域和预览的图片
-        preview.css("height", "0");
-        return;
-    }
-
+$("#imageSrc").on("change", function(){
+	if (window.FileReader) {
+			var reader = new FileReader();
+	} else {
+			alert("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
+	}
+	// Get a reference to the fileList
+	var files = !!this.files ? this.files : [];
+	
+	var preview = $("#td-form-field-preview");
+	// If no files were selected, or no FileReader support, return
+	if (!files.length){
+		alert("请选择文件！"); 
+		//没有选择文件则要清空文件上一次load的文件域和预览的图片
+		preview.css("height","0");
+		return;
+	}
     // Only proceed if the selected file is an image
-    if (/^image/.test(files[0].type)) {
-
+    if (/^image/.test( files[0].type)){
+ 
         var reader = new FileReader();
         // Read the local file as a DataURL
         reader.readAsDataURL(files[0]);
-
+ 
         // When loaded, set image data as background of div
-        reader.onloadend = function() {
-            preview.css("height", "80px");
-            preview.attr("src", this.result);
+        reader.onloadend = function(){
+			preview.css("height","80px");
+			preview.attr("src", this.result);        
         }
-
-    } else {
-        alert("请选择图片！");
-        return;
-    }
+ 
+    }else{
+		alert("请选择图片！"); 
+		var inputFile = $("#imageSrc");
+		inputFile.after(inputFile.clone().val(""));
+		inputFile.remove();
+		return;
+	}
 });
 
 $('#td-add-submit').click(function(e) {
@@ -44,8 +47,11 @@ $('#td-add-submit').click(function(e) {
         data: formData,
         processData: false, // 告诉jQuery不要去处理发送的数据
         contentType: false, // 告诉jQuery不要去设置Content-Type请求头
-        success: function() {
-            alert('恭喜！添加成功！');
+        success: function(data) {
+			console.log(data);
+			if(!data.error)
+				alert('恭喜！添加成功！');
+			else alert('ERROR！添加失败！');
         },
         error: function() {
             alert('很遗憾！添加失败！');
