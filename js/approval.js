636bf17ed1a89dffcfad5fe8b9522ac7252847a2
mj <603535738@qq.com>
2017-03-18 10:43:53 +0800

    1.修复approval.js里不能排布两个相同审批人bug。2.优化减少代码。3.解决approval.html界面审批线悬空问题

--- js/approval.js
+++ js/approval.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
 	var thisUser = localStorage.getItem('thisUser');
 
-	console.log(thisUser);
-
 	if( !thisUser ){
 		window.location.href = 'http://www.gdrtc.org/dd-verification/index.php';
 	}
@@ -19,22 +17,6 @@ $(document).ready(function(){
 	}
 });
 
-$.fn.setData = function(jsonValue){
-  var obj = this;
-  var findobj;
-  $.each(jsonValue,function(name,ival){
-	findobj = obj.find("[id="+name+"]")[0];
-	if(findobj){
-		findobj.textContent=ival;
-	}
-  })
-}
-function getUrlParam(){
-	var thisURL = decodeURI( document.URL );
-	//split("=")将url分为两部分，取第二部分
-	var a = thisURL.split("=")[1].split("&")[0];
-	return a;
-}
 function getJson(param,thisUser)
 {
 	$.ajax({
@@ -45,28 +27,27 @@ function getJson(param,thisUser)
 		dataType: 'json',
 		cache: false,
 		success: function(data) {
-			if(!data.error){
+			console.log(data);
+			if(data.error==0){
 				var result = data.records[0];
 				if(result.status!="0") {$('.td-approval-button-div').css("display","none");}
-				var usage = '';
 				switch(result.usage){
 					case "0":
-						usage="出差";
+						result.usage="出差";
 						break;
 					case "1":
-						usage="接待";
+						result.usage="接待";
 						break;
 					case "2":
-						usage="外勤";
+						result.usage="外勤";
 						break;
 					case "3":
-						usage="车辆维修";
+						result.usage="车辆维修";
 						break;
 					case "4":
-						usage="其他";
+						result.usage="其他";
 						break;
 				}
-				result.usage = usage;
 				result.plateNumber = result.car.plateNumber;
 				result.model = result.car.model;
 				result.driver = result.driver.name;
@@ -82,11 +63,12 @@ function getJson(param,thisUser)
 					html_resultinfo='';
 					html_resultinfo += '<div class="cd-timeline-block"><div class="cd-timeline-img cd-picture"><img src="'+
 						item['approver'].avatar+'" alt="Picture"></div><div class="cd-timeline-content"><h2>'+
-						item['approver'].name + '</h2><span class="cd-date" id="cd-date-'+item['userid']+'">' + item['operateDt'] +
-						'</span><p class="approval-result" id="approval-result-'+item['userid']+'">'+item["result"] +
-						' </p><span class="approval-hidden">'+item['sequence']+'</span></div></div>';
+						item['approver'].name + '</h2><span class="cd-date">' + item['operateDt'] +
+						'</span><p class="approval-result" id="approval-result-'+i+'">'+item["result"] +
+						' </p><span class="approval-sequence">'+item['sequence']+'</span><span class="approval-userid">'+
+						item['userid']+'</span></div></div>';
 					$('.cd-container').append(html_resultinfo);//after方法:在每个匹配的元素之后插入内容。
-					var setResult = $('#approval-result-'+item['userid']);
+					var setResult = $('#approval-result-'+i);
 					switch(item["result"]){
 						case "0":
 							setResult.text("未审批");
@@ -106,9 +88,9 @@ function getJson(param,thisUser)
 			}
 			var sign = getApproval();
 			if(sign == -1){
-				console.log("error");
+				console.log("error!no approver!");
 			}else{
-				var approvalId = $(appDiv[sign]).find(".cd-date")[0].id.slice(8);
+				var approvalId = $(appDiv[sign]).find(".approval-userid")[0].textContent;
 				console.log(approvalId+','+thisUser);
 				if(approvalId!=thisUser)
 				$('.td-approval-button-div').css("display","none");
@@ -116,102 +98,70 @@ function getJson(param,thisUser)
 
 		},
 		error:function(xhr,textStatus){
-		console.log('错误');
+		alert("很遗憾！传送错误");
 		console.log(xhr);
 		console.log(textStatus);
 		}
 	});
 }
 $('#td-agree-submit').click(function(e) {
-	var sign = getApproval();
-	if(sign == -1){
-		console.log("error");
-	}else{
-		var sequence = $(appDiv[sign]).find(".approval-hidden")[0].textContent;
-		var time = $(appDiv[sign]).find(".cd-date")[0];
-		var send={"sequence":sequence, "resid":getUrlParam(), "result":"1", "userid":time.id.slice(8)};
-		showMask();
-		$.ajax({
-        url: '../server/approval/approve.php',
-        type: "POST",
-        data: send,
-        cache: false,
-        success: function(data) {
-			if(!data.error){
-				var myDate = new Date();
-				time.textContent=myDate.toLocaleTimeString();     //获取当前时间
-				$(appDiv[sign]).find(".approval-result")[0].textContent = "已同意";
-				$(appDiv[sign]).find(".approval-result").css("color","green");
-				$('.td-approval-button-div').css("display","none");
-			}else{
-				alert('修改审批状态失败！'+data.errorMsg);
-			}
-			$("#td-mask").hide();
-        },
-        error: function() {
-            alert('很遗憾！审批失败！');
-        },
-        xhr: function () {
-            var xhr = new window.XMLHttpRequest();
-            xhr.upload.addEventListener("progress", function (e) {
-                console.log(e.lengthComputable);
-                if (e.lengthComputable) {
-                  100 * e.loaded / e.total;
-                }
-            }, false);
-            return xhr;
-        },
-    });
-	}
+	setApproval("1");
 
 });
 $('#td-disagree-submit').click(function(e) {
-	var sign = getApproval();
-	if(sign == -1){
-		console.log("error");
-	}else{
-		var sequence = $(appDiv[sign]).find(".approval-hidden")[0].textContent;
-		var time = $(appDiv[sign]).find(".cd-date")[0];
-
-		var send={"sequence":sequence, "resid":getUrlParam(), "result":"2", "userid":time.id.slice(8)};
-		showMask();
-		$.ajax({
-        url: '../server/approval/approve.php',
-        type: "POST",
-        data: send,
-        cache: false,
-        success: function(data) {
-			if(!data.error){
-				var myDate = new Date();
-				time.textContent=myDate.toLocaleTimeString();     //获取当前时间
-				$(appDiv[sign]).find(".approval-result")[0].textContent = "已拒绝";
-				$(appDiv[sign]).find(".approval-result").css("color","red");
-				$('.td-approval-button-div').css("display","none");
-			}else{
-				alert('修改审批状态失败！'+data.errorMsg);
-			}
-			$("#td-mask").hide();
-        },
-        error: function() {
-            alert('很遗憾！审批失败！');
-        },
-        xhr: function () {
-            var xhr = new window.XMLHttpRequest();
-            xhr.upload.addEventListener("progress", function (e) {
-                console.log(e.lengthComputable);
-                if (e.lengthComputable) {
-                  100 * e.loaded / e.total;
-                }
-            }, false);
-            return xhr;
-        },
-    });
-	}
-
+	setApproval("2");
 });
 
+function setApproval(appResult){
+	var sign = getApproval();
+	if(sign == -1){
+		console.log("error!no approver!");
+	}else{
+		var sequence = $(appDiv[sign]).find(".approval-sequence")[0].textContent;
+		var time = $(appDiv[sign]).find(".cd-date")[0];
+		var send={"sequence":sequence, "resid":getUrlParam(), "result":appResult, "userid":$(appDiv[sign]).find(".approval-userid")[0].textContent};
+		showMask();
+		$.ajax({
+			url: '../server/approval/approve.php',
+			type: "POST",
+			data: send,
+			cache: false,
+			success: function(data) {
+				if(!data.error){
+					var myDate = new Date();
+					time.textContent=myDate.toLocaleTimeString();     //获取当前时间
+					if(appResult == "1"){
+						$(appDiv[sign]).find(".approval-result")[0].textContent = "已同意";
+						$(appDiv[sign]).find(".approval-result").css("color","green");
+					}else{
+						$(appDiv[sign]).find(".approval-result")[0].textContent = "已拒绝";
+						$(appDiv[sign]).find(".approval-result").css("color","red");
+					}
+					$('.td-approval-button-div').css("display","none");
+				}else{
+					alert('修改审批状态失败！'+data.errorMsg);
+				}
+				$("#td-mask").hide();
+			},
+			error: function() {
+				alert('很遗憾！审批失败！');
+			},
+			xhr: function () {
+				var xhr = new window.XMLHttpRequest();
+				xhr.upload.addEventListener("progress", function (e) {
+					console.log(e.lengthComputable);
+					if (e.lengthComputable) {
+					  100 * e.loaded / e.total;
+					}
+				}, false);
+				return xhr;
+			},
+		});
+	}
+}
+
 function getApproval(){
-	 appDiv=$("section>div");
+	appDiv=$("section>div");
 	var sign=-1;
 	for(var i=0; i<appDiv.length; i++)
 	{
@@ -221,6 +171,24 @@ function getApproval(){
 		}
 	}
 	return sign;
+}
+
+$.fn.setData = function(jsonValue){
+  var obj = this;
+  var findobj;
+  $.each(jsonValue,function(name,ival){
+	findobj = obj.find("[id="+name+"]")[0];
+	if(findobj){
+		findobj.textContent=ival;
+	}
+  })
+}
+
+function getUrlParam(){
+	var thisURL = decodeURI( document.URL );
+	//split("=")将url分为两部分，取第二部分
+	var a = thisURL.split("=")[1].split("&")[0];
+	return a;
 }
 
 function showMask(){
